be302382786756f6febebb5ef20894eef586ee6a
Natalie Orsi <thisis@natalies.email>
2021-05-21 17:12:52 -0700

    Tests were not synced properly with git

--- project/simplemesh/tests.py
+++ project/simplemesh/tests.py
@@ -1,13 +1,14 @@
 import datetime
+from django.http import request
 from django.shortcuts import get_object_or_404
 from django.test import TestCase
 from django.utils import timezone
 
 from .models import Conversation, Message, Thought
-
+from .views import search
 # Run tests by navigating to manage.py and running `python manage.py test simplemesh`
 
-class ConversationModelTests(TestCase):
+class AllTests(TestCase):
     def test_create_conversation(self):
         now = timezone.now()
         text = "Auto test: Creating a conversation"
@@ -18,35 +19,47 @@ class ConversationModelTests(TestCase):
         self.assertIs(retrieved_type, Conversation)
   
     def test_create_message(self):
-        
-          # Assuming ID 1 exists; in reality it would be preferable to search and select 
-         # a conversation to add to, or create one in the test
         now = timezone.now()
 
         c = Conversation.objects.create(
-            
+            start_date=now,
+            text = "Auto test: Creating a conversation"
         )
-        c.start_date = c.startdate if c.startdate else now
-        c.text = c.text if c.text else "Auto test: Creating a conversation"
-  
-        text = "Auto test: Creating a message"
-        m=c.message_set.create(text=text, pub_date=now)
         c.save()
+
+        m=c.message_set.create(
+            pub_date=now,
+            text = "Auto test: Creating a message"
+        )
         m.save()
         retrieved_type = type(get_object_or_404(Message, id=m.id))
     
         # if I had more time I would clean up all these test data after the assert
-        self.assertIs(type(retrieved_type),Message)
+        self.assertIs(retrieved_type,Message)
   
     def test_create_thought(self):
-          # Assuming ID 1 exists; in reality it would be preferable to search and select 
-         # a message to add to, or create one in the test
         now = timezone.now()
-        text = "Auto test: Creating a thought"
-        m = get_object_or_404(Message, id=1)
-        t=m.thought_set.create(text=text, pub_date=now)
+
+        c = Conversation.objects.create(
+            start_date=now,
+            text = "Auto test: Creating a conversation"
+        )
+        c.save()
+
+        m=c.message_set.create(
+            pub_date=now,
+            text = "Auto test: Creating a message"
+        )
+        m.save()
+
+        t=m.thought_set.create(
+            pub_date=now,
+            text = "Auto test: Creating a thought"
+        )
+        t.save()
+        
         retrieved_type = type(get_object_or_404(Thought, id=t.id))
-        self.assertIs(type(retrieved_type),Thought)
+        self.assertIs(retrieved_type,Thought)
         
     # other tests if I had the time:
     # test empty string input for each type of model
